eac8c0ea7a1e3f3cab49508a48e21dee18e94cd3
Maxim Efimov <efimov.top.academy@gmail.com>
2024-10-21 19:30:52 +0300

    Добавьте файлы проекта.

---
+++ WpfBindingsApp/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBindingsApp
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        string name = "";
+        int age;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if(name != value)
+                {
+                    name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if(age != value)
+                {
+                    age = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfBindingsApp/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBindingsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Employee employee;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Binding binding = new Binding();
+            //binding.ElementName = "txtBox";
+            //binding.Path = new PropertyPath("Text");
+            //binding.Mode = BindingMode.TwoWay;
+            //txtBlock.SetBinding(TextBlock.TextProperty, binding);
+
+            employee = new Employee() { Name = "Bobby", Age = 32 };
+            
+            this.DataContext = employee;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //txtBlock.Text = "Hello world";
+            employee.Name = "Tommy";
+            employee.Age = 33;
+
+            MessageBox.Show(employee.ToString());
+        }
+    }
+}
